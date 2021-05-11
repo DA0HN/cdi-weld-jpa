e6fdec19731c25b71404fd281e4ae7964cc5d644
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-11 13:32:22 -0400

    feat: adiciona entidade User

---
+++ src/main/java/br/com/gabriel/models/User.java
@@ -0,0 +1,59 @@
+package br.com.gabriel.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * Created by daohn on 11/05/2021
+ * @author daohn
+ * @since 11/05/2021
+ */
+@Entity
+public class User implements Serializable {
+  // https://phauer.com/2016/uuids-hibernate-mysql/
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(columnDefinition = "BINARY(16)")
+  private UUID id;
+  private String name;
+  private String email;
+
+  public User() {
+  }
+
+  public User(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
