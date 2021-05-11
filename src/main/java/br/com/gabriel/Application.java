3df2a4e6151ed8cc5365f81c597dbdc341d2e9b3
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-11 08:15:41 -0400

    chore: estrutura inicial do projeto

---
+++ src/main/java/br/com/gabriel/Application.java
@@ -0,0 +1,12 @@
+package br.com.gabriel;
+
+/**
+ * Created by daohn on 10/05/2021
+ * @author daohn
+ * @since 10/05/2021
+ */
+public class Application {
+  public static void main(String[] args) {
+
+  }
+}
