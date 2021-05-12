42289a24d52563f56f6388e545635cfdd589b72a
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-12 17:48:20 -0400

    fix: corrige o producer de EntityManager

--- src/main/java/br/com/gabriel/config/producers/EntityManagerProducer.java
+++ src/main/java/br/com/gabriel/config/producers/EntityManagerProducer.java
@@ -2,9 +2,10 @@ package br.com.gabriel.config.producers;
 
 import br.com.gabriel.config.annotations.MySQLDatabase;
 
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.Persistence;
 
 /**
  * Created by daohn on 11/05/2021
@@ -13,7 +14,15 @@ import javax.persistence.PersistenceContext;
  */
 public class EntityManagerProducer {
   @Produces
-  @PersistenceContext(unitName = "cdi-weld")
   @MySQLDatabase
-  private EntityManager entityManager;
+  public EntityManager createEntityManager() {
+    return Persistence
+             .createEntityManagerFactory("cdi-weld")
+             .createEntityManager();
+  }
+
+  public void close(
+    @Disposes @MySQLDatabase EntityManager entityManager) {
+    entityManager.close();
+  }
 }
