e8abdc887bc8c8a491b79a37b05e99a3b2b1cd60
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-12 17:09:35 -0400

    feat: adiciona chamada da classe de serviço

--- src/main/java/br/com/gabriel/Application.java
+++ src/main/java/br/com/gabriel/Application.java
@@ -1,12 +1,24 @@
 package br.com.gabriel;
 
+import br.com.gabriel.service.UserService;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
 /**
  * Created by daohn on 10/05/2021
  * @author daohn
  * @since 10/05/2021
  */
 public class Application {
-  public static void main(String[] args) {
 
+  public static void main(String[] args) throws Exception {
+    Weld weld = new Weld();
+    WeldContainer container = weld.initialize();
+    UserService userService = container.getBeanManager()
+                                .createInstance()
+                                .select(UserService.class)
+                                .get();
+    userService.run();
+    weld.shutdown();
   }
 }
