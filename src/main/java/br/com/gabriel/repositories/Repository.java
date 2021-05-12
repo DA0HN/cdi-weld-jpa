664e5f67869c2eb715119247398fa3072a89700d
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-12 16:26:25 -0400

    refactor: refatora para interface de um repository

--- src/main/java/br/com/gabriel/repositories/Repository.java
+++ src/main/java/br/com/gabriel/repositories/Repository.java
@@ -1,22 +1,21 @@
 package br.com.gabriel.repositories;
 
-import br.com.gabriel.config.annotations.MySQLDatabase;
-
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 /**
  * Created by daohn on 11/05/2021
  * @author daohn
  * @since 11/05/2021
  */
-public class Repository {
+public interface Repository<T> {
 
-  private final EntityManager manager;
-
-  @Inject
-  public Repository(@MySQLDatabase EntityManager manager) {
-    this.manager = manager;
-  }
-
+  T findById(UUID id);
+  List<T> findByName(String name);
+  List<T> findAll();
+  void update(UUID id, Consumer<T>... updates) throws Exception;
+  void save(T entity);
+  void remove(UUID id);
+  String getTableName();
 }
